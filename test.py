02f9ccf1e1eeee09657fb7fdeeba95042987deea
SJTU910091 <59047327+SJTU910091@users.noreply.github.com>
2020-01-06 18:12:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
 import tensorflow as tf
 from keras.callbacks import Callback
-from keras.layers import (Conv3D, BatchNormalization, AveragePooling3D, concatenate, Lambda,Activation, Input, GlobalAvgPool3D, Dense)
+from keras.layers import (Lambda, AveragePooling3D, Conv3D, BatchNormalization, concatenate, Input, GlobalAvgPool3D, Activation, Dense)
 from keras.regularizers import l2 as l2l
 from keras.models import Model
 config = tf.ConfigProto()
